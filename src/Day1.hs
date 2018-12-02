952164eeff0ae20440a0e39c636cb39ea073a193
andrewMacmurray <a.macmurray@icloud.com>
2018-12-02 11:10:19 +0000

    adds part 2 day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,15 +1,33 @@
 module Day1
-  ( solution
+  ( solution1
+  , solution2
   ) where
 
 import           Data.List (lines)
+import qualified Data.Set  as Set
 import           System.IO (readFile)
 
-solution :: IO Int
-solution = frequency <$> readFile "resources/day-1-frequency.txt"
+solution1 :: IO Int
+solution1 = sum <$> frequencies
 
-frequency :: String -> Int
-frequency = sum . fmap parseFrequency . lines
+solution2 :: IO Int
+solution2 = findDuplicate Set.empty 0 . cycle <$> frequencies
+
+findDuplicate :: Set.Set Int -> Int -> [Int] -> Int
+findDuplicate seen current (x:xs) =
+  if appearsTwice
+    then frequency
+    else findDuplicate frequencies frequency xs
   where
-    parseFrequency ('+':n) = read n
-    parseFrequency n       = read n
+    frequency = current + x
+    appearsTwice = Set.member frequency seen
+    frequencies = Set.insert frequency seen
+
+frequencies :: IO [Int]
+frequencies = fmap frequency . lines <$> fromFile
+  where
+    frequency ('+':n) = read n
+    frequency n       = read n
+
+fromFile :: IO String
+fromFile = readFile "resources/day-1-frequency.txt"
--- src/Main.hs
+++ src/Main.hs
@@ -3,4 +3,11 @@ module Main where
 import qualified Day1
 
 main :: IO ()
-main = Day1.solution >>= print
+main = do
+  printSolution 1 1 Day1.solution1
+  printSolution 1 2 Day1.solution2
+
+printSolution :: Show a => Int -> Int -> IO a -> IO ()
+printSolution day number solution = putStr message >> solution >>= print
+  where
+    message = mconcat ["Day ", show day, ", Solution ", show number, ": "]
